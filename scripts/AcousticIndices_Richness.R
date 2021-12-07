5cf1be780484aa357d61e5cd170f8b43eb906be8
Slade Allen-Ankins <jc696551@ad.jcu.edu.au>
2021-12-07 16:18:45 +1000

    Added code to handle Shannon's Diversity Index

--- scripts/AcousticIndices_Richness.R
+++ scripts/AcousticIndices_Richness.R
@@ -32,45 +32,51 @@ Plots_Indices_Richness <- list()
 Models_Indices_Richness <- list()
 Bootstrap_Indices_Richness <- list()
 
-IndicesRichness_Combinations <- expand.grid(Indices = grep("mean", colnames(acousticIndices_richness), value = TRUE),
-                                            type = unique(acousticIndices_richness$type))
+IndicesRichness_Combinations <- expand.grid(index = grep("mean", colnames(acousticIndices_richness), value = TRUE),
+                                            taxa = unique(acousticIndices_richness$type),
+                                            diversity = c("richness", "shannon"))
 
 for (combination in 1:nrow(IndicesRichness_Combinations)) {
-  currentAcousticIndex <- IndicesRichness_Combinations$Indices[combination]
-  currentRichness <- IndicesRichness_Combinations$type[combination]
+  currentAcousticIndex <- IndicesRichness_Combinations$index[combination]
+  currentTaxa <- IndicesRichness_Combinations$taxa[combination]
+  currentDiversity <- IndicesRichness_Combinations$diversity[combination]
   
-  data <- acousticIndices_richness %>% filter(type == currentRichness)
+  data <- acousticIndices_richness %>% filter(type == currentTaxa)
   
   #Mixed-effects model with 'Site' as random effect
-  Models_Indices_Richness[[paste0(currentAcousticIndex, "_", currentRichness)]] <- lmer(as.formula(paste0("richness ~ ", currentAcousticIndex, " + (1|Site/Sensor) + (1|sampling.period)")), 
-                                                                                        data = data)
+  Models_Indices_Richness[[paste0(currentDiversity, "_", currentAcousticIndex, "_", currentTaxa)]] <- lmer(as.formula(paste0(currentDiversity, " ~ ", currentAcousticIndex, " + (1|Site/Sensor) + (1|sampling.period)")), 
+                                                                                                                                  data = data)
   
   #Plot and save visual check of model assumptions
-  png(filename = paste0("./outputs/model.checks/", currentAcousticIndex, "_", currentRichness, ".png"), width = 30, height = 20, units = "cm", res = 800)
-  print(performance::check_model(Models_Indices_Richness[[paste0(currentAcousticIndex, "_", currentRichness)]]))
+  png(filename = paste0("./outputs/model.checks/", currentDiversity, "_", currentAcousticIndex, "_", currentTaxa, ".png"), width = 30, height = 20, units = "cm", res = 800)
+  print(performance::check_model(Models_Indices_Richness[[paste0(currentDiversity, "_", currentAcousticIndex, "_", currentTaxa)]]))
   dev.off()
   
   #Model-based (Semi-)Parametric Bootstrap for Mixed Models - Used to calculate confidence intervals
-  Bootstrap_Indices_Richness[[paste0(currentAcousticIndex, "_", currentRichness)]] <- bootMer(Models_Indices_Richness[[paste0(currentAcousticIndex, "_", currentRichness)]],
-                                                                                              FUN = function(x)predict(x, re.form=NA),
-                                                                                              nsim = 1000)
+  Bootstrap_Indices_Richness[[paste0(currentDiversity, "_", currentAcousticIndex, "_", currentTaxa)]] <- bootMer(Models_Indices_Richness[[paste0(currentDiversity, "_", currentAcousticIndex, "_", currentTaxa)]],
+                                                                                                                 FUN = function(x)predict(x, re.form=NA),
+                                                                                                                 nsim = 1000)
   
-  data$lci <- apply(Bootstrap_Indices_Richness[[paste0(currentAcousticIndex, "_", currentRichness)]]$t, 2, quantile, 0.025)
-  data$uci <- apply(Bootstrap_Indices_Richness[[paste0(currentAcousticIndex, "_", currentRichness)]]$t, 2, quantile, 0.975)
-  data$pred <- predict(Models_Indices_Richness[[paste0(currentAcousticIndex, "_", currentRichness)]], re.form=NA)
+  data$lci <- apply(Bootstrap_Indices_Richness[[paste0(currentDiversity, "_", currentAcousticIndex, "_", currentTaxa)]]$t, 2, quantile, 0.025)
+  data$uci <- apply(Bootstrap_Indices_Richness[[paste0(currentDiversity, "_", currentAcousticIndex, "_", currentTaxa)]]$t, 2, quantile, 0.975)
+  data$pred <- predict(Models_Indices_Richness[[paste0(currentDiversity, "_", currentAcousticIndex, "_", currentTaxa)]], re.form=NA)
   
-  ylimits <- if(currentRichness == 'birds') {c(10, 50)} else {c(0, 12)}
+  ylimits <- if(currentTaxa == 'birds' & currentDiversity == 'richness') {c(10, 50)} else 
+    if(currentTaxa == 'frogs' & currentDiversity == 'richness') {c(0, 12)} else
+      if(currentTaxa == 'birds' & currentDiversity == 'shannon') {c(1.8, 4.2)} else 
+        if(currentTaxa == 'frogs' & currentDiversity == 'shannon') {c(0, 1.2)}
+  
   
   #Plot of fixed effects and confidence intervals
-  Plots_Indices_Richness[[paste0(currentAcousticIndex, "_", currentRichness)]] <- data %>% 
-    ggplot(aes_string(x = paste0(currentAcousticIndex), y = "richness")) + 
+  Plots_Indices_Richness[[paste0(currentDiversity, "_", currentAcousticIndex, "_", currentTaxa)]] <- data %>% 
+    ggplot(aes_string(x = paste0(currentAcousticIndex), y = paste0(currentDiversity))) + 
     geom_ribbon(aes_string(x = paste0(currentAcousticIndex), ymin = "lci", ymax = "uci"), fill = "black", alpha = 0.1) +
     geom_line(aes_string(x = paste0(currentAcousticIndex), y = "pred"), color = "black", lwd = 1) +
     geom_point(size = 2) +
     scale_y_continuous(limits = ylimits) + 
     annotate(geom = "text", -Inf, Inf, hjust = -0.1, vjust = 1.8, parse = T,
-             label = paste0("R^2 == ", format(round(as.numeric(performance::r2(Models_Indices_Richness[[paste0(currentAcousticIndex, "_", currentRichness)]])[[2]]), 2), nsmall = 2))) +
-    labs(x = gsub("_mean", "", paste0(currentAcousticIndex)), y = "Species Richness") +
+             label = paste0("R^2 == ", format(round(as.numeric(performance::r2(Models_Indices_Richness[[paste0(currentDiversity, "_", currentAcousticIndex, "_", currentTaxa)]])[[2]]), 2), nsmall = 2))) +
+    labs(x = gsub("_mean", "", paste0(currentAcousticIndex)), y = "Diversity Measure") +
     theme_classic() +
     theme(axis.title.y = element_blank())
 }
@@ -78,26 +84,18 @@ for (combination in 1:nrow(IndicesRichness_Combinations)) {
 
 # Arrange multi-panel plots -----------------------------------------------
 
-#Plot acoustic indices vs bird richness
-Plot_Indices_Birds <- plot_grid(plotlist = Plots_Indices_Richness[grep("birds", names(Plots_Indices_Richness))],
-                                ncol = 4) %>% annotate_figure(top = "lmer - AcousticIndicesMorning_BirdRichness")
+plot_combinations <- unique(IndicesRichness_Combinations[,c('diversity','taxa')])
 
-Plot_Indices_Birds <- annotate_figure(Plot_Indices_Birds,
-                                      left = "Species Richness",
-                                      bottom = "Acoustic Index")
-
-ggsave(filename = paste0("./outputs/figures/", Sys.Date(), "Plots_BirdRichness_MorningIndicesMean_lmer.png"),
-       plot = Plot_Indices_Birds,
-       width = 30, height = 25, units = "cm", dpi = 1200)
-
-#Plot acoustic indices vs frog richness
-Plot_Indices_Frogs <- plot_grid(plotlist = Plots_Indices_Richness[grep("frogs", names(Plots_Indices_Richness))],
-                                ncol = 4) %>% annotate_figure(top = "lmer - AcousticIndicesEvening_FrogRichness")
-
-Plot_Indices_Frogs <- annotate_figure(Plot_Indices_Frogs,
-                                      left = "Species Richness",
-                                      bottom = "Acoustic Index")
-
-ggsave(filename = paste0("./outputs/figures/", Sys.Date(), "Plots_FrogRichness_EveningIndicesMean_lmer.png"),
-       plot = Plot_Indices_Frogs,
-       width = 30, height = 25, units = "cm", dpi = 1200)
+for (combination in 1:nrow(plot_combinations)) {
+  ytitle <- ifelse(plot_combinations$diversity[combination] == 'richness', "Species Richness", "Shannon Diversity")
+  
+  Plot <- plot_grid(plotlist = Plots_Indices_Richness[grep(paste0(plot_combinations$diversity[combination], ".*", plot_combinations$taxa[combination]), names(Plots_Indices_Richness))],
+                    ncol = 4) %>% 
+    annotate_figure(top = paste0("lmer - AcousticIndices_", plot_combinations$taxa[combination], "_", plot_combinations$diversity[combination]),
+                    left = ytitle,
+                    bottom = "Acoustic Index")
+  
+  ggsave(filename = paste0("./outputs/figures/", Sys.Date(), "Plots_", plot_combinations$taxa[combination], "_", plot_combinations$diversity[combination], ".png"),
+         plot = Plot,
+         width = 30, height = 25, units = "cm", dpi = 1200)
+}

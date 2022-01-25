bf3418aac0efd946cf74ff35ea212b67d637f24f
Slade Allen-Ankins <jc696551@ad.jcu.edu.au>
2022-01-25 16:47:19 +1000

    updated glmmTMB script - fixed index values to 0-1 and 'censored' them

--- scripts/AcousticIndices_Biodiversity_glmmTMB.R
+++ scripts/AcousticIndices_Biodiversity_glmmTMB.R
@@ -1,16 +1,62 @@
-#glmmTMB
+#Correlate acoustic indices with vertebrate diversity - 2021
 
+# Load packages -----------------------------------------------------------
+
+library(tidyverse)
+library(ggpubr)
+library(cowplot)
+library(lme4)
+library(lmerTest)
+library(boot)
 library(glmmTMB)
 
-testData <- acousticIndices_richness[acousticIndices_richness$type == 'birds',]
 
+# Load indices and richness data, merge -----------------------------------
+
+setwd("C:/Users/jc696551/OneDrive - James Cook University/Projects/acousticindices_vertebratediversity")
+
+load("./outputs/data/2021-12-06_acousticIndices_summary.RData") #load indices
+
+richness <- read_csv("./rawdata/biodiversity/richness_diversity_updated.csv") #load richness
+
+#format richness to match indices
+richness <- richness %>% rename(Site = site, Sensor = original.plot, richness = Richness, shannon = Shannon, type = taxa)
+#richness$Sensor <- gsub(" ", "", richness$Sensor) #now redundant?
+richness <- richness %>% mutate(sampling.period = paste0(season, ".2021"))
+richness <- richness %>% mutate(type = recode(type,
+                                              'bird' = 'birds', 'frog' = 'frogs'))
+
+#duplicate indices 'all' and rename to 'not.birds' for comparison to all other species
+acousticIndices_summary <- rbind(acousticIndices_summary, 
+                                 acousticIndices_summary %>% filter(type == 'all') %>% mutate(type = recode(type, 'all' = 'not.birds')))
+
+#merge richness and acoustic indices
+acousticIndices_richness <- merge(acousticIndices_summary, richness[richness$day == 'all',], by = c("type", "Site", "Sensor", "sampling.period"))
+
+
+#scale all indices to 0-1 and 'censor' min and max values
 range01 <- function(x){(x-min(x))/(max(x)-min(x))}
 
-testData$ACI_mean <- range01(testData$ACI_mean)
+acousticIndices_richness <- acousticIndices_richness %>% 
+  mutate_at(vars(matches("_mean")), range01) %>% 
+  mutate_at(vars(matches("_mean")), ~ replace(.x, which(.x == min(.x)), 0.00001)) %>% 
+  mutate_at(vars(matches("_mean")), ~ replace(.x, which(.x == max(.x)), 0.99999))
 
-#'censor' min and max values to not be 0 or 1
-testData$ACI_mean[which.min(testData$ACI_mean)] <- 0.00001
-testData$ACI_mean[which.max(testData$ACI_mean)] <- 0.99999
+
+
+# Mixed effects glmmTMB models --------------------------------------------
+
+Plots_Indices_Richness <- list()
+Models_Indices_Richness <- list()
+Bootstrap_Indices_Richness <- list()
+
+IndicesRichness_Combinations <- expand.grid(index = grep("mean", colnames(acousticIndices_richness), value = TRUE),
+                                            taxa = unique(acousticIndices_richness$type),
+                                            diversity = c("richness", "shannon", "count"))
+
+############UP TO HERE##############
+
+
 
 glmmTMB_model_richness <- glmmTMB(ACI_mean ~ richness + (1|Site) + (1|sampling.period),
                                   data = testData,

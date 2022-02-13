d6b5404c7d2c3f1448dbd93981afea28929dc874
Slade Allen-Ankins <jc696551@ad.jcu.edu.au>
2022-02-14 09:42:46 +1000

    Added bootstrap correlation code for looking at different ACI bands, birds_all vs birds_day vs birds_morning, and frogs_night vs frogs_evening; readme update

--- scripts/BootstrapCorrelationPlots.R
+++ scripts/BootstrapCorrelationPlots.R
@@ -9,28 +9,57 @@ library(lmerTest)
 library(boot)
 
 
-# Load indices and richness data, merge -----------------------------------
+# Load indices and biodiversity data --------------------------------------
 
 setwd("C:/Users/jc696551/OneDrive - James Cook University/Projects/acousticindices_vertebratediversity")
 
-load("./outputs/data/2021-12-06_acousticIndices_summary.RData") #load indices
+#summary indices
+files <- file.info(list.files("./outputs/data/", pattern = ".*_acousticIndices_summary.RData$", full.names = TRUE)) #list files
+latestFile <- rownames(files)[which.max(files$mtime)] #determine most recent file to use for loading
 
+load(latestFile)
+
+#richness
 richness <- read_csv("./rawdata/biodiversity/richness_diversity_updated.csv") #load richness
 
 #format richness to match indices
 richness <- richness %>% rename(Site = site, Sensor = original.plot, richness = Richness, shannon = Shannon, type = taxa)
-#richness$Sensor <- gsub(" ", "", richness$Sensor) #now redundant?
 richness <- richness %>% mutate(sampling.period = paste0(season, ".2021"))
 richness <- richness %>% mutate(type = recode(type,
                                               'bird' = 'birds', 'frog' = 'frogs'))
 
-#duplicate indices 'all' and rename to 'not.birds' for comparison to all other species
-acousticIndices_summary <- rbind(acousticIndices_summary, 
-                                 acousticIndices_summary %>% filter(type == 'all') %>% mutate(type = recode(type, 'all' = 'not.birds')))
+# Merge indices and biodiversity data -------------------------------------
 
-#merge richness and acoustic indices
-acousticIndices_richness <- merge(acousticIndices_summary, richness[richness$day == 'all',], by = c("type", "Site", "Sensor", "sampling.period"))
+#function to combine indices (all, day, night, morning, evening) and biodiversity (all, not.birds, birds, frogs) based on user specified combinations
+combineIndicesBiodiversity <- function(indices,
+                                       richness,
+                                       combinations) {
+  frames <- list()
+  for (combination in 1:length(combinations)) {
+    frames[[combination]] <- merge(indices %>% 
+                                     filter(type == combinations[[combination]][1]) %>% 
+                                     mutate(type = case_when(type == combinations[[combination]][1] ~ names(combinations[combination]))),
+                                   richness %>% 
+                                     filter(day == 'all') %>% 
+                                     filter(type == combinations[[combination]][2]) %>% 
+                                     mutate(type = case_when(type == combinations[[combination]][2] ~ names(combinations[combination]))),
+                                   by = c("type", "Site", "Sensor", "sampling.period"))
+  }
+  frames <- do.call(rbind, frames)
+  
+  return(frames)
+}
 
+#combine indices and biodiversity using above function
+acousticIndices_richness <- combineIndicesBiodiversity(indices = acousticIndices_summary,
+                                                       richness = richness,
+                                                       combinations = list(all_all = c('all', 'all'),
+                                                                           not.birds_all = c('all', 'not.birds'),
+                                                                           birds_morning = c('morning', 'birds'),
+                                                                           birds_day = c('day', 'birds'),
+                                                                           birds_all = c('all', 'birds'),
+                                                                           frogs_evening = c('evening', 'frogs'),
+                                                                           frogs_night = c('night', 'frogs')))
 
 # Calculate bootstrap correlation between indices and biodiversity --------
 
@@ -38,10 +67,10 @@ bootCor_results <- list()
 
 for (measure in c('count', 'richness', 'shannon')) {
   
-  for (taxa in c('all', 'birds', 'frogs', 'not.birds')) {
+  for (comparison in unique(acousticIndices_richness$type)) {
     
     for (index in colnames(select(acousticIndices_richness, ends_with(c("mean"))))) {
-      bootResults <- boot(acousticIndices_richness[acousticIndices_richness$type == taxa,], 
+      bootResults <- boot(acousticIndices_richness[acousticIndices_richness$type == comparison,], 
                           statistic = function(data, i) {
                             cor(data[i, measure], data[i, index], method='pearson')
                           },
@@ -49,8 +78,8 @@ for (measure in c('count', 'richness', 'shannon')) {
       bootResultsCI <- boot.ci(bootResults, 
                                conf = 0.95, type = "bca")
       
-      bootCor_results[[paste(measure, taxa, index, sep = "_")]] <- data.frame(Index = gsub("_mean", "", index),
-                                                                              Taxa = taxa,
+      bootCor_results[[paste(measure, comparison, index, sep = "_")]] <- data.frame(Index = gsub("_mean", "", index),
+                                                                              Taxa = comparison,
                                                                               Measure = measure,
                                                                               Mean = mean(bootResults$t),
                                                                               Low = bootResultsCI$bca[4],
@@ -64,6 +93,105 @@ bootCor_results <- do.call(rbind, bootCor_results)
 
 # Plot correlation bootstrap results --------------------------------------
 
+#correlation plot for birds with different ACI frequency bands
+indicesToUse <- c("ACI", 
+                  "ACI_1000_4000", "ACI_3000_6000", "ACI_5000_8000",
+                  "ACI_1000_2000", "ACI_2000_3000", "ACI_3000_4000", "ACI_4000_5000",
+                  "ACI_5000_6000", "ACI_6000_7000", "ACI_7000_8000")
+
+correlationPlots_birdsACI <- list()
+for (measure in c('richness', 'shannon', 'count')) {
+  tmp_data <- bootCor_results[bootCor_results$Measure == measure & 
+                                bootCor_results$Index %in% indicesToUse,]
+  tmp_data <- tmp_data[grep("^birds*", tmp_data$Taxa),]
+  tmp_data$Index <- fct_relevel(tmp_data$Index, indicesToUse)
+  
+  correlationPlots_birdsACI[[measure]] <- ggplot(data = tmp_data, aes(x = Mean, y = Index, group = Taxa, colour = Taxa)) +
+    geom_vline(xintercept = 0, linetype = 'dashed') +
+    geom_pointrange(aes(xmin = Low, xmax = High), position = position_dodge(width = 0.4)) +
+    geom_hline(yintercept = 1+0.5, linetype = "dotted") +
+    geom_hline(yintercept = 4+0.5, linetype = "dotted") +
+    scale_x_continuous(limits = c(-0.9, 0.9), breaks = seq(-0.8, 0.8, 0.4)) +
+    scale_color_viridis_d() +
+    labs(x = "Mean correlation") +
+    theme_classic() +
+    theme(axis.title = element_blank(),
+          legend.position = "none")
+}
+
+legend_bottom <- get_legend(
+  correlationPlots_birdsACI[['count']] + 
+    guides(color = guide_legend(nrow = 1)) +
+    theme(legend.position = "bottom", legend.direction = "horizontal", legend.title = element_blank())
+)
+
+correlationPlot_birdsACI <- plot_grid(plotlist = correlationPlots_birdsACI,
+                             ncol = 1,
+                             labels = c(paste0("a) ", names(correlationPlots_birdsACI)[1]),
+                                        paste0("b) ", names(correlationPlots_birdsACI)[2]),
+                                        paste0("c) ", names(correlationPlots_birdsACI)[3])),
+                             hjust = 0, label_x = 0.23) %>% 
+  annotate_figure(left = "Acoustic index", bottom = "Mean correlation") %>% 
+  plot_grid(legend_bottom, ncol = 1, rel_heights = c(1, .1))
+
+ggsave("outputs/figures/bootstrapcorrelations/bootstrap_correlations_birdsACI.png",
+       correlationPlot_birdsACI,
+       width = 14, height = 24, units = "cm", dpi = 800)
+
+
+#correlation plot for frogs using indices for evening and night
+indicesToUse <- c("ACI", 
+                  "ADI", 
+                  "AEI", 
+                  "BI", 
+                  "NDSI", 
+                  "EVN",
+                  "SH",
+                  "LFC",
+                  "MFC",
+                  "HFC")
+
+correlationPlots_frogs <- list()
+for (measure in c('richness', 'shannon', 'count')) {
+  tmp_data <- bootCor_results[bootCor_results$Measure == measure & 
+                                bootCor_results$Index %in% indicesToUse,]
+  tmp_data <- tmp_data[grep("^frogs*", tmp_data$Taxa),]
+  tmp_data$Index <- fct_relevel(tmp_data$Index, indicesToUse)
+  
+  correlationPlots_frogs[[measure]] <- ggplot(data = tmp_data, aes(x = Mean, y = Index, group = Taxa, colour = Taxa)) +
+    geom_vline(xintercept = 0, linetype = 'dashed') +
+    geom_pointrange(aes(xmin = Low, xmax = High), position = position_dodge(width = 0.4)) +
+    scale_x_continuous(limits = c(-0.9, 0.9), breaks = seq(-0.8, 0.8, 0.4)) +
+    scale_color_viridis_d() +
+    labs(x = "Mean correlation") +
+    theme_classic() +
+    theme(axis.title = element_blank(),
+          legend.position = "none")
+}
+
+legend_bottom <- get_legend(
+  correlationPlots_frogs[['count']] + 
+    guides(color = guide_legend(nrow = 1)) +
+    theme(legend.position = "bottom", legend.direction = "horizontal", legend.title = element_blank())
+)
+
+correlationPlot_frogs <- plot_grid(plotlist = correlationPlots_frogs,
+                                   ncol = 1,
+                                   labels = c(paste0("a) ", names(correlationPlots_frogs)[1]),
+                                              paste0("b) ", names(correlationPlots_frogs)[2]),
+                                              paste0("c) ", names(correlationPlots_frogs)[3])),
+                                   hjust = 0, label_x = 0.12) %>% 
+  annotate_figure(left = "Acoustic index", bottom = "Mean correlation") %>% 
+  plot_grid(legend_bottom, ncol = 1, rel_heights = c(1, .1))
+
+ggsave("outputs/figures/bootstrapcorrelations/bootstrap_correlations_frogs.png",
+       correlationPlot_frogs,
+       width = 12, height = 24, units = "cm", dpi = 800)
+
+
+
+
+#correlation plot for all comparisons using standard indices (i.e. not frequency specific ACI bands)
 indicesToUse <- c("ACI", 
                   "ADI", 
                   "AEI", 
@@ -80,7 +208,7 @@ for (measure in c('richness', 'shannon', 'count')) {
     geom_vline(xintercept = 0, linetype = 'dashed') +
     geom_pointrange(aes(xmin = Low, xmax = High), position = position_dodge(width = 0.4)) +
     scale_x_continuous(limits = c(-0.9, 0.9), breaks = seq(-0.8, 0.8, 0.4)) +
-    scale_color_viridis_d(labels = c('All vertebrates', 'Birds', 'Frogs', 'Not birds')) +
+    #scale_color_viridis_d(labels = c('All vertebrates', 'Birds', 'Frogs', 'Not birds')) +
     labs(x = "Mean correlation") +
     theme_classic() +
     theme(axis.title = element_blank(),

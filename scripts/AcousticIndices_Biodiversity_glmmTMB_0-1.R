d79e51495cbdc85b81fe528b90b1a0a1ca441af5
Slade Allen-Ankins <jc696551@ad.jcu.edu.au>
2022-03-18 16:43:56 +1000

    Partial update of glmmTMB_0-1 script to work with latest data and new indices

--- scripts/AcousticIndices_Biodiversity_glmmTMB_0-1.R
+++ scripts/AcousticIndices_Biodiversity_glmmTMB_0-1.R
@@ -10,29 +10,57 @@ library(lmerTest)
 library(boot)
 library(glmmTMB)
 
+# Load indices and biodiversity data --------------------------------------
 
-# Load indices and richness data, merge -----------------------------------
+#summary indices
+files <- file.info(list.files("./outputs/data/weekly_summaries/", pattern = ".*_acousticIndices_summary.RData$", full.names = TRUE)) #list files
+latestFile <- rownames(files)[which.max(files$mtime)] #determine most recent file to use for loading
 
-setwd("C:/Users/jc696551/OneDrive - James Cook University/Projects/acousticindices_vertebratediversity")
+load(latestFile)
 
-load("./outputs/data/2021-12-06_acousticIndices_summary.RData") #load indices
-
+#richness
 richness <- read_csv("./rawdata/biodiversity/richness_diversity_updated.csv") #load richness
 
 #format richness to match indices
-richness <- richness %>% rename(Site = site, Sensor = original.plot, richness = Richness, shannon = Shannon, type = taxa)
-#richness$Sensor <- gsub(" ", "", richness$Sensor) #now redundant?
+richness <- richness %>% rename(Site = site, Sensor = original.plot, richness = Richness, shannon = Shannon, type = comparison)
 richness <- richness %>% mutate(sampling.period = paste0(season, ".2021"))
 richness <- richness %>% mutate(type = recode(type,
                                               'bird' = 'birds', 'frog' = 'frogs'))
 
-#duplicate indices 'all' and rename to 'not.birds' for comparison to all other species
-acousticIndices_summary <- rbind(acousticIndices_summary, 
-                                 acousticIndices_summary %>% filter(type == 'all') %>% mutate(type = recode(type, 'all' = 'not.birds')))
+# Merge indices and biodiversity data -------------------------------------
 
-#merge richness and acoustic indices
-acousticIndices_richness <- merge(acousticIndices_summary, richness[richness$day == 'all',], by = c("type", "Site", "Sensor", "sampling.period"))
+#function to combine indices (all, day, night, morning, afternoon, evening) and biodiversity (all, not.birds, birds, frogs) based on user specified combinations
+combineIndicesBiodiversity <- function(indices,
+                                       richness,
+                                       combinations) {
+  frames <- list()
+  for (combination in 1:length(combinations)) {
+    frames[[combination]] <- merge(indices %>% 
+                                     filter(type == combinations[[combination]][1]) %>%
+                                     mutate(type = case_when(type == combinations[[combination]][1] ~ names(combinations[combination]))),
+                                   richness %>% 
+                                     filter(day == 'all') %>% 
+                                     filter(type == combinations[[combination]][2]) %>% 
+                                     mutate(type = case_when(type == combinations[[combination]][2] ~ names(combinations[combination]))),
+                                   by = c("type", "Site", "Sensor", "sampling.period"))
+  }
+  frames <- do.call(rbind, frames)
+  
+  return(frames)
+}
 
+#combine indices and biodiversity using above function
+acousticIndices_richness <- combineIndicesBiodiversity(indices = acousticIndices_summary,
+                                                       richness = richness,
+                                                       combinations = list(all_all = c('all', 'all'),
+                                                                           not.birds_all = c('all', 'not.birds'),
+                                                                           birds_morning = c('morning', 'birds'),
+                                                                           birds_afternoon = c('afternoon', 'birds'),
+                                                                           birds_day = c('day', 'birds'),
+                                                                           birds_all = c('all', 'birds'),
+                                                                           frogs_evening = c('evening', 'frogs'),
+                                                                           frogs_night = c('night', 'frogs')))
+acousticIndices_richness <- acousticIndices_richness %>% filter(p > 0.7) #remove datapoints where less than 70% of audio was available
 
 #scale all indices to 0-1 and 'censor' min and max values
 range01 <- function(x){(x-min(x))/(max(x)-min(x))}
@@ -45,39 +73,44 @@ acousticIndices_richness <- acousticIndices_richness %>%
 
 # Mixed effects glmmTMB models --------------------------------------------
 
+#indicesToUse <- c('ACI', 'ADI', 'AEI', 'BI', 'NDSI', 'EVN', 'SH', 'LFC', 'MFC', 'HFC') #specify which indices to use in models
+indicesToUse <- c('ADI', 'AEI', 'BI', 'NDSI', 'SH', 
+                  'Activity', 'EventsPerSecond', 'LowFreqCover', 'MidFreqCover', 'HighFreqCover', 
+                  'AcousticComplexity', 'ClusterCount', 'SptDensity')
+
 Plots_Indices_Richness <- list()
 Models_Indices_Richness <- list()
 Bootstrap_Indices_Richness <- list()
 
-IndicesRichness_Combinations <- expand.grid(index = grep("mean", colnames(acousticIndices_richness), value = TRUE),
-                                            taxa = unique(acousticIndices_richness$type),
-                                            diversity = c("richness", "shannon", "count"))
+IndicesRichness_Combinations <- expand.grid(index = grep("mean", colnames(acousticIndices_richness), value = TRUE)[which(gsub("_mean","",grep("mean", colnames(acousticIndices_richness), value = TRUE)) %in% indicesToUse)],
+                                            comparison = c("all_all", "not.birds_all", "birds_day", "frogs_night"),
+                                            measure = c("richness", "shannon", "count"))
 
 pb = txtProgressBar(min = 0, max = nrow(IndicesRichness_Combinations), initial = 0, style = 3) #progress bar for loop
 
 for (combination in 1:nrow(IndicesRichness_Combinations)) {
   currentAcousticIndex <- IndicesRichness_Combinations$index[combination]
-  currentTaxa <- IndicesRichness_Combinations$taxa[combination]
-  currentDiversity <- IndicesRichness_Combinations$diversity[combination]
+  currentComparison <- IndicesRichness_Combinations$comparison[combination]
+  currentMeasure <- IndicesRichness_Combinations$measure[combination]
   
-  data <- acousticIndices_richness %>% filter(type == currentTaxa)
+  data <- acousticIndices_richness %>% filter(type == currentComparison)
   
-  Models_Indices_Richness[[paste0(currentDiversity, "_", currentAcousticIndex, "_", currentTaxa)]] <- glmmTMB(as.formula(paste0(currentAcousticIndex, " ~ ", currentDiversity, " + (1|Site) + (1|sampling.period)")),
+  Models_Indices_Richness[[paste0(currentMeasure, "_", currentAcousticIndex, "_", currentComparison)]] <- glmmTMB(as.formula(paste0(currentAcousticIndex, " ~ ", currentMeasure, " + (1|Site) + (1|sampling.period)")),
                                                                                                               data = data,
                                                                                                               family = beta_family())
   
-  Bootstrap_Indices_Richness[[paste0(currentDiversity, "_", currentAcousticIndex, "_", currentTaxa)]] <- bootMer(Models_Indices_Richness[[paste0(currentDiversity, "_", currentAcousticIndex, "_", currentTaxa)]],
+  Bootstrap_Indices_Richness[[paste0(currentMeasure, "_", currentAcousticIndex, "_", currentComparison)]] <- bootMer(Models_Indices_Richness[[paste0(currentMeasure, "_", currentAcousticIndex, "_", currentComparison)]],
                                                                                                                  FUN = function(x)predict(x, re.form=NA, type = "response"),
                                                                                                                  nsim = 100)
   
-  data$lci <- apply(Bootstrap_Indices_Richness[[paste0(currentDiversity, "_", currentAcousticIndex, "_", currentTaxa)]]$t, 2, quantile, 0.025, na.rm = TRUE)
-  data$uci <- apply(Bootstrap_Indices_Richness[[paste0(currentDiversity, "_", currentAcousticIndex, "_", currentTaxa)]]$t, 2, quantile, 0.975, na.rm = TRUE)
-  data$pred <- predict(Models_Indices_Richness[[paste0(currentDiversity, "_", currentAcousticIndex, "_", currentTaxa)]], re.form=NA, type = "response")
+  data$lci <- apply(Bootstrap_Indices_Richness[[paste0(currentMeasure, "_", currentAcousticIndex, "_", currentComparison)]]$t, 2, quantile, 0.025, na.rm = TRUE)
+  data$uci <- apply(Bootstrap_Indices_Richness[[paste0(currentMeasure, "_", currentAcousticIndex, "_", currentComparison)]]$t, 2, quantile, 0.975, na.rm = TRUE)
+  data$pred <- predict(Models_Indices_Richness[[paste0(currentMeasure, "_", currentAcousticIndex, "_", currentComparison)]], re.form=NA, type = "response")
   
-  Plots_Indices_Richness[[paste0(currentDiversity, "_", currentAcousticIndex, "_", currentTaxa)]] <- data %>% 
-    ggplot(aes_string(x = paste0(currentDiversity), y = paste0(currentAcousticIndex))) +
-    geom_ribbon(aes_string(x = paste0(currentDiversity), ymin = "lci", ymax = "uci"), fill = "black", alpha = 0.1) +
-    geom_line(aes_string(x = paste0(currentDiversity), y = "pred"), color = "black", lwd = 1) +
+  Plots_Indices_Richness[[paste0(currentMeasure, "_", currentAcousticIndex, "_", currentComparison)]] <- data %>% 
+    ggplot(aes_string(x = paste0(currentMeasure), y = paste0(currentAcousticIndex))) +
+    geom_ribbon(aes_string(x = paste0(currentMeasure), ymin = "lci", ymax = "uci"), fill = "black", alpha = 0.1) +
+    geom_line(aes_string(x = paste0(currentMeasure), y = "pred"), color = "black", lwd = 1) +
     geom_point(size = 2) +
     scale_y_continuous(limits = c(0, 1), 
                        labels = scales::label_number(accuracy = 0.1),
@@ -91,21 +124,21 @@ for (combination in 1:nrow(IndicesRichness_Combinations)) {
 
 # Arrange multi-panel plots -----------------------------------------------
 
-plot_combinations <- unique(IndicesRichness_Combinations[,c('diversity','taxa')])
+plot_combinations <- unique(IndicesRichness_Combinations[,c('measure','comparison')])
 
 for (combination in 1:nrow(plot_combinations)) {
   
-  xtitle <- if(plot_combinations$diversity[combination] == 'richness') {c("Species Richness")} else
-    if(plot_combinations$diversity[combination] == 'shannon') {c("Shannon Diversity")} else
-      if(plot_combinations$diversity[combination] == 'count') {c("Total Abundance")}
+  xtitle <- if(plot_combinations$measure[combination] == 'richness') {c("Species Richness")} else
+    if(plot_combinations$measure[combination] == 'shannon') {c("Shannon Diversity")} else
+      if(plot_combinations$measure[combination] == 'count') {c("Total Abundance")}
   
-  Plot <- plot_grid(plotlist = Plots_Indices_Richness[grep(paste0(plot_combinations$diversity[combination], ".*mean_", plot_combinations$taxa[combination]), names(Plots_Indices_Richness))],
+  Plot <- plot_grid(plotlist = Plots_Indices_Richness[grep(paste0(plot_combinations$measure[combination], ".*mean_", plot_combinations$comparison[combination]), names(Plots_Indices_Richness))],
                     ncol = 4) %>% 
-    annotate_figure(top = paste0("glmmTMB_beta - AcousticIndices_", plot_combinations$taxa[combination], "_", plot_combinations$diversity[combination]),
+    annotate_figure(top = paste0("glmmTMB_beta - AcousticIndices_", plot_combinations$comparison[combination], "_", plot_combinations$measure[combination]),
                     bottom = xtitle,
                     left = "Acoustic Index")
   
-  ggsave(filename = paste0("./outputs/figures.local/glmmTMB_beta_0-1/", Sys.Date(), "Plots_", plot_combinations$taxa[combination], "_", plot_combinations$diversity[combination], ".png"),
+  ggsave(filename = paste0("./outputs/figures.local/glmmTMB_beta_0-1/", Sys.Date(), "Plots_", plot_combinations$comparison[combination], "_", plot_combinations$measure[combination], ".png"),
          plot = Plot,
          width = 30, height = 25, units = "cm", dpi = 1200)
 }
@@ -120,17 +153,17 @@ indicesToUse <- c("ACI",
 
 for (combination in 1:nrow(plot_combinations)) {
   
-  xtitle <- if(plot_combinations$diversity[combination] == 'richness') {c("Species Richness")} else
-    if(plot_combinations$diversity[combination] == 'shannon') {c("Shannon Diversity")} else
-      if(plot_combinations$diversity[combination] == 'count') {c("Total Abundance")}
+  xtitle <- if(plot_combinations$measure[combination] == 'richness') {c("Species Richness")} else
+    if(plot_combinations$measure[combination] == 'shannon') {c("Shannon Diversity")} else
+      if(plot_combinations$measure[combination] == 'count') {c("Total Abundance")}
   
-  Plot <- plot_grid(plotlist = Plots_Indices_Richness[grep(paste0(plot_combinations$diversity[combination], "_(", paste0(indicesToUse, collapse = "|"), ")_.*mean_", plot_combinations$taxa[combination]), names(Plots_Indices_Richness), value = T)],
+  Plot <- plot_grid(plotlist = Plots_Indices_Richness[grep(paste0(plot_combinations$measure[combination], "_(", paste0(indicesToUse, collapse = "|"), ")_.*mean_", plot_combinations$comparison[combination]), names(Plots_Indices_Richness), value = T)],
                     ncol = 3) %>% 
-    annotate_figure(top = paste0("glmmTMB_beta - AcousticIndices_", plot_combinations$taxa[combination], "_", plot_combinations$diversity[combination]),
+    annotate_figure(top = paste0("glmmTMB_beta - AcousticIndices_", plot_combinations$comparison[combination], "_", plot_combinations$measure[combination]),
                     bottom = xtitle,
                     left = "Acoustic Index")
   
-  ggsave(filename = paste0("./outputs/figures/glmmTMB_beta_0-1/", plot_combinations$taxa[combination], "_", plot_combinations$diversity[combination], ".png"),
+  ggsave(filename = paste0("./outputs/figures/glmmTMB_beta_0-1/", plot_combinations$comparison[combination], "_", plot_combinations$measure[combination], ".png"),
          plot = Plot,
          width = 22.5, height = 12.5, units = "cm", dpi = 1200)
 }
